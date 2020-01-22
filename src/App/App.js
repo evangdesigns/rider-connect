91001e924e24c31b141c025e95c1045ff7283030
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-21 19:15:35 -0600

    completed navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../helpers/data/connection';
 
+import Navbar from '../components/shared/Navbar/Navbar';
 import './App.scss';
 
+firebaseConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
   render() {
     return (
       <div className="App">
-        <button className="btn btn-warning">RiderConnect</button>
+        <Navbar />
       </div>
     );
   }
